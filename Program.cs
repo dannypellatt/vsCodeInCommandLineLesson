13ca48fae629487cffb811c64e5b87e31b0a86f3
dannypellatt <danielpellatt92@gmail.com>
2022-06-30 11:33:16 -0400

    Changed hello world t be a little Freddy

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I see a little silhouetto of a man");
+Console.WriteLine("Scaramouche!");
+Console.WriteLine("Scaramouche!");
+Console.WriteLine("Can you do the fandango?");
